b0d960a685f8d990ba73e247850a542214818848
iheb <iheb11ben@gmail.com>
2022-10-03 12:23:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rs';
+  elementType= NgxQrcodeElementTypes.URL
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH
+  value = ''
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 501;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a6');
+      let position = 50;
+      PDF.addImage(FILEURI, 'PNG', 35, position, fileWidth, fileHeight);
+      PDF.save('Qr-Code.pdf');
+    });
+  }
 }
